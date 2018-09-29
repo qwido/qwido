2c5fb151e06972808b1d7dc4e703aa8a65170a70
Kaven Thériault <zoid@leetzone.ca>
2018-09-29 10:14:37 -0400

    add bottom sheet to main page

--- lib/src/pages/capture_page.dart
+++ lib/src/pages/capture_page.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:qwido/utils.dart';
 
-class CapturePage extends StatelessWidget {
+class CapturePage extends StatefulWidget {
+  @override
+  _CapturePageState createState() => _CapturePageState();
+}
+
+class _CapturePageState extends State<CapturePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  VoidCallback _showBottomSheetCallback;
+
+  @override
+  void initState() {
+    super.initState();
+    _showBottomSheetCallback = _showBottomSheet;
+  }
+
+  void _showBottomSheet() {
+    final controller = TextEditingController();
+    setState(() {
+      _showBottomSheetCallback = null;
+    });
+    _scaffoldKey.currentState
+        .showBottomSheet<Null>((BuildContext context
+        ) {
+          final ThemeData themeData = Theme.of(context);
+          return Container(
+            decoration: BoxDecoration(
+                border:
+                    Border(top: BorderSide(color: themeData.disabledColor))),
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Code Qwido',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: controller,
+                onEditingComplete: () => Navigator.push(
+                  context,
+                  RoutingAssistant.navToArtworkPage(
+                    ArtId(controller.text),
+                  ),
+                ),
+              ),
+            ),
+          );
+        })
+        .closed
+        .whenComplete(() {
+          if (mounted) {
+            setState(() {
+              _showBottomSheetCallback = _showBottomSheet;
+            });
+          }
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Qwido"),
       ),
@@ -39,22 +97,9 @@ class CapturePage extends StatelessWidget {
                 bottom: 20.0,
               ),
             ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Code Qwido',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-              ),
-              keyboardType: TextInputType.numberWithOptions(),
-              controller: controller,
-              onEditingComplete: () => Navigator.push(
-                    context,
-                    RoutingAssistant.navToArtworkPage(
-                      ArtId(controller.text),
-                    ),
-                  ),
-            ),
+            RaisedButton(
+                onPressed: _showBottomSheetCallback,
+                child: const Text('SHOW BOTTOM SHEET')),
           ],
         ),
       ),
