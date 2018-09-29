c183c93f6b28b9a7a7081c61f66edadd990e728e
Joel Trottier-Hebert <iamquebecois@gmail.com>
2018-09-29 08:58:05 -0400

    Formatting

--- lib/src/pages/capture_page.dart
+++ lib/src/pages/capture_page.dart
@@ -6,57 +6,58 @@ class CapturePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = TextEditingController();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Qwido"),
+      appBar: AppBar(
+        title: Text("Qwido"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 100.0,
+          bottom: 0.0,
+          left: 24.0,
+          right: 24.0,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(
-            top: 100.0,
-            bottom: 0.0,
-            left: 24.0,
-            right: 24.0,
-          ),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                child: FlatButton(
-                  onPressed: () => scanQR().then((code) {
-                        if (code != null) {
-                          Navigator.push(context,
-                              RoutingAssistant.navToArtworkPage(ArtId(code)));
-                        }
-                      }),
-                  child: Icon(
-                    Icons.photo_camera,
-                    size: 250.0,
-                  ),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              child: FlatButton(
+                onPressed: () => scanQR().then((code) {
+                      if (code != null) {
+                        Navigator.push(context,
+                            RoutingAssistant.navToArtworkPage(ArtId(code)));
+                      }
+                    }),
+                child: Icon(
+                  Icons.photo_camera,
+                  size: 250.0,
                 ),
               ),
-              Padding(
-                child: Divider(),
-                padding: EdgeInsets.only(
-                  top: 100.0,
-                  bottom: 20.0,
+            ),
+            Padding(
+              child: Divider(),
+              padding: EdgeInsets.only(
+                top: 100.0,
+                bottom: 20.0,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Code Qwido',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
                 ),
               ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Code Qwido',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-                keyboardType: TextInputType.numberWithOptions(),
-                controller: controller,
-                onEditingComplete: () => Navigator.push(
-                      context,
-                      RoutingAssistant.navToArtworkPage(
-                        ArtId(controller.text),
-                      ),
+              keyboardType: TextInputType.numberWithOptions(),
+              controller: controller,
+              onEditingComplete: () => Navigator.push(
+                    context,
+                    RoutingAssistant.navToArtworkPage(
+                      ArtId(controller.text),
                     ),
-              ),
-            ],
-          ),
-        ));
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
