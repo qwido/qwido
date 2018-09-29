2eeebd7f4549c6fc03e601cda77692fd5800b216
Kim Desrosiers <kimdesro@gmail.com>
2018-09-29 00:08:36 -0400

    Update artwork_page.dart

--- lib/src/pages/artwork_page.dart
+++ lib/src/pages/artwork_page.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ArtworkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView(
+    return ListView(
       children: <Widget>[
-        new Column(
+        Column(
           children: <Widget>[
-            new Container(
-              child: new Image.asset("lib/assets/artwork-placeholder.png",
+            Container(
+              child: Text(
+                "Corot: Women",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+            ),
+            Container(
+              child: Image.asset("lib/assets/artwork-placeholder.png",
                   height: 300.00),
               padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
             ),
-            new Text("Coucher de soleil avec manathan ensoleillé"),
-            new Text("Paul Girard"),
-            new Container(
-              child: new Text(
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text(
+                'Paul Girard',
+                style: Theme.of(context).textTheme.subhead,
+              ),
+            ),
+            Container(
+              child: Text(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada cursus lacinia. Vestibulum auctor imperdiet condimentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vitae mi eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam in hendrerit magna. Ut odio metus, tincidunt.",
                 textAlign: TextAlign.justify,
               ),
               padding: EdgeInsets.only(left: 10.0, right: 10.0),
             ),
-            new IconButton(
+            IconButton(
                 // Use the FontAwesomeIcons class for the IconData
-                icon: new Icon(Icons.share),
+                icon: Icon(Icons.share),
                 onPressed: () {
                   print("Pressed");
                 }),
