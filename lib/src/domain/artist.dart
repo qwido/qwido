098628ede9da13d045a78c8ec01e7c25ad6df1f9
Kim Desrosiers <kimdesro@gmail.com>
2018-09-29 14:14:46 -0400

    Add the domain package

---
+++ lib/src/domain/artist.dart
@@ -0,0 +1,9 @@
+class Artist {
+  String id;
+  String name;
+  String photo;
+  String bio;
+  List<String> oeuvres;
+
+  Artist({this.id, this.name, this.photo, this.bio, this.oeuvres});
+}
---
+++ lib/src/domain/artworkRepository.dart
@@ -0,0 +1,7 @@
+import 'package:qwido/domain.dart';
+
+class ArtworkRepository {
+  ArtworkRepository() {}
+
+  Artwork findById(String id) {}
+}
