3241cf0dbfb8268c1f8b65d77513eebe258349e3
Joel Trottier-Hebert <iamquebecois@gmail.com>
2018-09-29 21:22:59 -0400

    Rename to convention

--- lib/domain.dart
+++ lib/domain.dart
@@ -1,6 +1,6 @@
 export 'src/domain/artwork.dart';
 export 'src/domain/artist.dart';
-export 'src/domain/artistRepository.dart';
-export 'src/domain/artworkRepository.dart';
-export 'src/domain/mapMarkerRepository.dart';
+export 'src/domain/artist_repository.dart';
+export 'src/domain/artwork_repository.dart';
+export 'src/domain/map_marker_repository.dart';
 export 'src/domain/social.dart';
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,6 +1,5 @@
 export 'src/widgets/qwido_code_input.dart';
 export 'src/widgets/scan_code_button.dart';
 export 'src/widgets/artwork_carousel.dart';
-export 'src/widgets/ArtistListTile.dart';
 export 'src/widgets/social_list.dart';
 export 'src/widgets/artist_list_tile.dart';
